90578f8d91fb280857be41e88a3a4ef2845dfd56
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2021-06-18 18:33:46 +0200

    Add new slider

--- js/carousel.js
+++ js/carousel.js
@@ -1,88 +1,86 @@
 import {
     quote,
     author,
-    carouselImg,
     carouselCirclesContainer,
     carouselCircles,
+    imgContainer,
 } from './vars.js';
 import { DUMMY_DATA } from '../data/dummy_data.js';
 
 let counter = 0;
-let interuptor = false;
-
-carouselImg.classList.add('fadeIn');
+let newCounter = 1;
+let imgSlider = imgContainer.querySelector('#imgSlider');
 
 quote.innerText = DUMMY_DATA[0].quote;
 author.innerText = `${DUMMY_DATA[0].author}, ${DUMMY_DATA[0].jobTitle}`;
-carouselImg.src = `${DUMMY_DATA[0].img}`;
 
 DUMMY_DATA.forEach((element) => {
     let div = document.createElement('div');
     div.classList.add('carouselCircle');
     div.classList.add('noSelect');
     carouselCirclesContainer.appendChild(div);
+    let image = document.createElement('img');
+    image.src = `${element.img}`;
+    imgSlider.appendChild(image);
 });
-
 carouselCircles[0].classList.add('active');
+
+let images = imgSlider.querySelectorAll('img');
 
 const classRemover = () => {
     for (let i = 0; i < carouselCircles.length; i++) {
         carouselCircles[i].classList.remove('active');
     }
+};
 
-    if (interuptor) {
-        setTimeout(() => {
-            interuptor = false;
-        }, 2000);
-    } else {
-        carouselImg.classList.remove('fadeIn');
+const imageSliderFunction = () => {
+    imgSlider.style.transition = `transform .4s ease-in-out`;
+    imgSlider.style.transform =
+        'translateX(' + -images[0].clientWidth * newCounter + 'px';
+    newCounter++;
+    if (newCounter > images.length) {
+        newCounter = 1;
+        imgSlider.style.transform =
+            'translateX(' +
+            images[0].clientWidth / (images.length * 100) +
+            'px';
     }
 };
 
-document.addEventListener('click', (event) => {
-    if (
-        event.target.classList.contains('carouselCircle') &&
-        !event.target.classList.contains('active')
-    ) {
-        interuptor = true;
+// document.addEventListener('click', (event) => {
+//     if (
+//         event.target.classList.contains('carouselCircle') &&
+//         !event.target.classList.contains('active')
+//     ) {
 
-        classRemover();
+//         classRemover();
 
-        event.target.classList.add('active');
-        for (let i = 0; i < carouselCircles.length; i++) {
-            if (carouselCircles[i].classList.contains('active')) {
-                quote.innerText = DUMMY_DATA[i].quote;
-                author.innerText = `${DUMMY_DATA[i].author}, ${DUMMY_DATA[i].jobTitle}`;
-                carouselImg.src = `${DUMMY_DATA[i].img}`;
-                counter = i;
-            }
-        }
-    }
-});
+//         event.target.classList.add('active');
+//         for (let i = 0; i < carouselCircles.length; i++) {
+//             if (carouselCircles[i].classList.contains('active')) {
+//                 quote.innerText = DUMMY_DATA[i].quote;
+//                 author.innerText = `${DUMMY_DATA[i].author}, ${DUMMY_DATA[i].jobTitle}`;
+
+//                 counter = i;
+//             }
+//         }
+//     }
+// });
 
 export const automaticalSlideChange = () => {
     const changeCarouselContent = () => {
         quote.innerText = DUMMY_DATA[counter].quote;
         author.innerText = `${DUMMY_DATA[counter].author}, ${DUMMY_DATA[counter].jobTitle}`;
-        carouselImg.src = `${DUMMY_DATA[counter].img}`;
         classRemover();
-
         setTimeout(() => {
-            carouselImg.classList.add('fadeIn');
-        }, 2000);
-
+            imageSliderFunction();
+        }, 3000);
         carouselCircles[counter].classList.add('active');
         counter++;
         if (counter === DUMMY_DATA.length) {
             counter = 0;
         }
     };
-    const complete = () => {
-        clearInterval(timer);
-        timer = null;
-    };
 
-    let timer = setInterval(changeCarouselContent, 2000);
+    let timer = setInterval(changeCarouselContent, 3000);
 };
-
-// automaticalSlideChange();
--- js/vars.js
+++ js/vars.js
@@ -1,6 +1,9 @@
 export const quote = document.querySelector('q');
 export const author = document.querySelector('.author');
 export const carouselImg = document.getElementById('carouselImg');
+export const imgContainer = document.getElementById('imgContainer');
+export const nextBtn = document.getElementById('btnNext');
+export const prevBtn = document.getElementById('btnPrev');
 export const carouselElements = [quote, author, carouselImg];
 
 export const hamburger = document.getElementById('hamburger');
